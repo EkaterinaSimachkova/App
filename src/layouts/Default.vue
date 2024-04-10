075f390507d2d63e54a852f6a15ab1435138b9c2
Nikita Romanov <n.romanov@mkskom.ru>
2024-04-10 20:31:47 +0300

    feat: Пример default

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,17 @@
 <template>
-  <q-layout class="layout-default" view="hHh lpR lFf"> </q-layout>
+  <q-layout >
+    <q-header>
+      <slot name="header"></slot>
+    </q-header>
+    <q-page-container class="flex">
+      <router-view
+        class="layout-view col-grow"
+        :class="contentClass"
+        :key="$route.path"
+      />
+    </q-page-container>
+    <slot name="footer"></slot>
+  </q-layout>
 </template>
 
 <script setup></script>
